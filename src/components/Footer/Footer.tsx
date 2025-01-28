ee6f80511aec890d08944b6c9ff19693525638cd
kononoVikaReal <diler123500121+1@gmail.com>
2025-01-28 19:16:26 +0500

    The footer has been adapted

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,28 +4,52 @@ import styles from './Footer.module.css'
 export default function Footer() {
 	return (
 		<div className={styles.footer}>
-			<Image
-				className={styles.zapret}
-				src='/Zapret logo2.png'
-				alt='Zapret logo'
-				height={112}
-				width={230}
-			/>
+			<div className={styles.imageContainer1}>
+				<Image
+					className={styles.zapret}
+					src='/Zapret logo2.png'
+					alt='Zapret logo'
+					layout='responsive' // Используйте layout="responsive" для адаптивного изображения
+					height={112}
+					width={230}
+				/>
+			</div>
 			<div className={styles.social}>
 				<div className={styles.socialTitles}>
-					<Image
-						src='/telegram.png'
-						alt='telegram logo'
-						width={44}
-						height={44}
-					/>
-					<a target='_blank' href={'https://t.me/bypassblock'}>
+					<div className={styles.imageContainer2}>
+						<Image
+							className={styles.zapret}
+							src='/telegram.png'
+							alt='telegram logo'
+							layout='responsive' // Используйте layout="responsive" для адаптивного изображения
+							width={44}
+							height={44}
+						/>
+					</div>
+					<a
+						className={styles.a}
+						target='_blank'
+						href={'https://t.me/bypassblock'}
+					>
 						Новости
 					</a>
 				</div>
 				<div className={styles.socialTitles}>
-					<Image src='/github.png' alt='github logo' width={44} height={44} />
-					<a target='_blank' href={'https://github.com/youtubediscord/zapret'}>
+					<div className={styles.imageContainer3}>
+						<Image
+							className={styles.zapret}
+							src='/github.png'
+							alt='github logo'
+							layout='responsive' // Используйте layout="responsive" для адаптивного изображения
+							width={44}
+							height={44}
+						/>
+					</div>
+					<a
+						className={styles.a}
+						target='_blank'
+						href={'https://github.com/youtubediscord/zapret'}
+					>
 						Исходный код
 					</a>
 				</div>
