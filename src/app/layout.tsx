937a44cb915a751e53b04754ef425b2cb187c4d8
kononoVikaReal <diler123500121+1@gmail.com>
2025-01-29 21:25:29 +0500

    Metadata updated (opengraph, twitter)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,28 @@ export const metadata: Metadata = {
 		'Запрет — это инновационный метод обхода блокировок, который работает без изменения IP-адреса пользователя. В отличие от VPN, данный способ сохраняет вашу исходную геолокацию, что делает его незаметным для систем контроля и фильтрации трафика',
 	keywords:
 		'zapret, discord zapret, zapret youtube, zapret discord youtube, zapret github, zapret github discord? Zapret, Запрет, zapret, запрет, скачать запрет, скачать Zapret, zapret скачать, программа Zapret, программа Запрет, скачать VPN, бесплатный VPN, YouTube не работает, VPN для YouTube, VPN для Discord, YouTube VPN, Discord VPN, GoodBye DPI, бесплатные VPN, VPN не работает, vpn, скачать vpn, goodbyedpi, goodbydpi',
+	viewport: 'width=device-width, initial-scale=1.0',
+	openGraph: {
+		title: 'Zapret - обход блокировок',
+		description: 'Инновационный метод обхода блокировок - ПО Zapret',
+		url: 'https://zapret.vercel.app/',
+		siteName: 'Zapret - инновационный метод обхода блокировок',
+		images: [
+			{
+				url: 'https://zapret.vercel.app/Zapret bigLogo.png',
+				width: 800,
+				height: 600,
+			},
+		],
+		locale: 'ru_RU',
+		type: 'website',
+	},
+	twitter: {
+		card: 'Zapret - инновационный метод обхода блокировок',
+		title: 'Zapret - обход блокировок',
+		description: 'Инновационный метод обхода блокировок - ПО Zapret',
+		images: ['https://zapret.vercel.app/Zapret bigLogo.png'],
+	},
 }
 
 export default function RootLayout({
