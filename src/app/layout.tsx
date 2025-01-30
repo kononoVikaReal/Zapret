541e86fd8a26a4f5c14286345f402050f7dd35bb
kononoVikaReal <diler123500121+1@gmail.com>
2025-01-30 21:40:33 +0500

    updated icons and meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,24 +15,26 @@ const JetBrainsMono = JetBrains_Mono({
 export const metadata: Metadata = {
 	twitter: {
 		card: 'summary_large_image',
-		title: 'Запрет — инновационный метод обхода блокировок',
+		title:
+			'Запрет — инновационный метод обхода блокировок (дискорда, ютуба и тд)',
 		description:
 			'Запрет обход блокировки дискорда и ютуба. Помогает если ютуб и дискорд не работает или интернет шатдаун провайдера заблокирован сайты. zapret discord youtube',
 		creator: '@bypassblock',
 		images: ['https://zapret.vercel.app/Zapret bigLogo.png'],
 	},
-	title: 'Zapret',
+	title: 'Zapret - обход блокировок дискорда и ютуба (и иных блокировок)',
 	description:
 		'Запрет обход блокировки дискорда и ютуба. Помогает если ютуб и дискорд не работает или интернет шатдаун провайдера заблокирован сайты. zapret discord youtube',
 	keywords:
 		'zapret, discord zapret, zapret youtube, zapret discord youtube, zapret github, zapret github discord? Zapret, Запрет, zapret, запрет, скачать запрет, скачать Zapret, zapret скачать, программа Zapret, программа Запрет, скачать VPN, бесплатный VPN, YouTube не работает, VPN для YouTube, VPN для Discord, YouTube VPN, Discord VPN, GoodBye DPI, бесплатные VPN, VPN не работает, vpn, скачать vpn, goodbyedpi, goodbydpi',
 	viewport: 'width=device-width, initial-scale=1.0',
 	openGraph: {
-		title: 'Zapret - обход блокировок',
+		title: 'Zapret - обход блокировок (дискорда, ютуба и тд)',
 		description:
 			'Запрет обход блокировки дискорда и ютуба. Помогает если ютуб и дискорд не работает или интернет шатдаун провайдера заблокирован сайты. zapret discord youtube',
 		url: 'https://zapret.vercel.app/',
-		siteName: 'Zapret - инновационный метод обхода блокировок',
+		siteName:
+			'Zapret - инновационный метод обхода блокировок (дискорда, ютуба и тд)',
 		images: [
 			{
 				url: 'https://zapret.vercel.app/Zapret bigLogo.png',
@@ -52,6 +54,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='ru'>
+			<head>
+				<meta name='apple-mobile-web-app-title' content='Zapret' />
+			</head>
 			<body className={`${JetBrainsMono.className} antialiased`}>
 				<Navigation />
 				{children}
