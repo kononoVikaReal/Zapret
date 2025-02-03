7cf9ff15f433ac1cdd91062f9e395a4a7d7d0af1
kononoVikaReal <diler123500121+1@gmail.com>
2025-02-04 00:06:22 +0500

    updated scroll to download cards (by adding scroll function)

--- src/components/InfoDownload/InfoDownload.tsx
+++ src/components/InfoDownload/InfoDownload.tsx
@@ -1,11 +1,31 @@
+'use client'
 import Image from 'next/image'
-import Link from 'next/link'
 import styles from './InfoDownload.module.css'
+
+function scroll() {
+	const my_element = document.getElementById('download')
+
+	if (my_element) {
+		const elementPosition =
+			my_element.getBoundingClientRect().top + window.scrollY // Позиция элемента относительно документа
+		const offset = window.innerHeight / 2 - my_element.offsetHeight / 2 // Вычисляем смещение для центрирования
+		const scrollToPosition = elementPosition - offset // Конечная позиция скролла
+
+		window.scrollTo({
+			top: scrollToPosition,
+			behavior: 'smooth', // Плавный скролл
+		})
+	}
+}
+
 export default function InfoDownload() {
 	return (
 		<div className={styles.background}>
-			<div className={styles.download}>
-				<Link href='#download'>Скачать Zapret</Link>
+			<div
+				className={`${styles.download} cursor-pointer`}
+				onClick={() => scroll()}
+			>
+				<span>Скачать Zapret</span>
 			</div>
 			<div className={styles.instruction}>
 				<div className={styles.image}>
